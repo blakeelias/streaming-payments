2a68cee0dfba2141d1f498347d1ca80651bbb6c8
Blake Elias <blakeelias@gmail.com>
2022-05-07 20:59:12 +0000

    Testing client/server price request

--- server.py
+++ server.py
@@ -5,6 +5,12 @@ import socket
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
 
+
+object_prices = {
+    'object1': 10,
+    'object2': 20,
+}
+
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.bind((HOST, PORT))
     s.listen()
@@ -12,7 +18,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     with conn:
         print(f"Connected by {addr}")
         while True:
-            data = conn.recv(1024)
-            if not data:
+            request = conn.recv(1024)
+            if not request:
                 break
-            conn.sendall(data)
+            price = object_prices[request_obj]
+            conn.sendall(price)
